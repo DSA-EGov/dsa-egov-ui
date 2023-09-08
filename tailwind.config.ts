27c002833103c3ec1d6ed109ae2a1fb7dff50647
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2023-09-08 18:23:23 +0300

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['**/*.{ts,tsx,css,scss,sass,js,jsx,html}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
