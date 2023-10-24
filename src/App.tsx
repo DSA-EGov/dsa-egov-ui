8d9460955a5f6034b5479437871b7761bed5d822
Corneliu Nastas <cnastas@genevamedia.com>
2023-10-24 16:08:07 +0300

    feat: integrate keycloak.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
+import Login from './pages/Login.tsx';
+import keycloakConfig from '../utils/keycloakConfig.ts';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
 
 const App: FC = () => {
-  return <main className="text-red-600">123</main>;
+
+  return (
+    <ReactKeycloakProvider authClient={keycloakConfig} initOptions={{
+      onLoad: 'login-required',
+    }}>
+      <React.StrictMode>
+        <Login />
+      </React.StrictMode>
+    </ReactKeycloakProvider>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloakConfig from '../utils/keycloakConfig.ts';
+
 import App from './App.tsx';
+
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
