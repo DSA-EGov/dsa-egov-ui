8e2e7d5bb12ae24a4e76788a25e23e12470f75f3
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2023-10-30 23:19:10 +0200

    feat: update app

---
+++ src/icons/Plus.tsx
@@ -0,0 +1,7 @@
+import type { IconComponent } from '@/types/IconComponent'
+
+export const Plus: IconComponent = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+    <path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"></path>
+  </svg>
+);
